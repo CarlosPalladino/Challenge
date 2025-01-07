2fb75e8c6ad396693a695a9bc430c14247a4d9f7
carlos Palla <carlosezequiel19palladino@gmail.com>
2025-01-06 22:04:21 -0300

    Agregar archivos de proyecto.

---
+++ Data/Client.cs
@@ -0,0 +1,15 @@
+﻿namespace Data
+{
+    public class Client
+    {   
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Cuit { get; set; }
+        public int Dni { get; set; }
+        public DateTime  Nacimiento { get; set; }
+        public int Phone { get; set; }
+
+    }
+}
