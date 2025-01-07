6a551f4f580b191aa9c5ac298a2dbf48cf2abbdf
Carlos <carlosezequiel19palladino@gmail.com>
2025-01-07 12:17:37 -0300

    Organización en N capas hecho

--- Data/Entitie/Client.cs
+++ Data/Entitie/Client.cs
@@ -1,14 +1,14 @@
-﻿namespace Data
+﻿namespace Data.Entitie
 {
     public class Client
-    {   
-        public int Id { get; set; } 
+    {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public int Cuit { get; set; }
         public int Dni { get; set; }
-        public DateTime  Nacimiento { get; set; }
+        public DateTime Nacimiento { get; set; }
         public int Phone { get; set; }
 
     }
